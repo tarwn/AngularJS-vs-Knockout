201790fc8f13233684343f27a276e99614d95d73
Eli Weinstock-Herman <tarwn@tiernok.com>
2013-09-29 19:54:39 -0400

    Comparison samples started
    Samples for data binding, validation, serialization, templating, and modules and DI

---
+++ Knockout/js/AsyncDI/sampleServices/ListOfItemsService.js
@@ -0,0 +1,13 @@
+﻿// define a ListOfItemsService service that return a list of items when called
+define("sampleServices/ListOfItemsService", function () {
+    // return an object literal for the service object
+    return {
+        getList: function () {
+            return [
+                { name: "first value", number: 123 },
+                { name: "second value", number: 456 },
+                { name: "third value", number: 789 }
+            ];
+        }
+    }
+});
