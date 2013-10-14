c5d5641f68102eccff2454cabf89112bce878fa5
Eli Weinstock-Herman <tarwn@tiernok.com>
2013-10-14 11:13:35 -0400

    Replace files I accidentally removed

---
+++ Angular/js/AsyncDI/sampleApp.js
@@ -0,0 +1,13 @@
+﻿// and a module that has a controller that depends on the ListOfItemsService
+var sampleApp = angular.module('sampleApp', ['sampleServices']);
+sampleApp.controller('ModuleDIController',
+    ['$scope', 'ListOfItemsService',
+    function ($scope, listOfItemsService) {
+        $scope.textValue = "some text";
+        $scope.listOfItems = [];
+
+        $scope.fillItems = function () {
+            $scope.listOfItems = listOfItemsService.getList();
+        };
+    }]
+);
---
+++ Angular/js/AsyncDI/sampleServices.js
@@ -0,0 +1,12 @@
+﻿// define a module with a sample ListOfItemsService that return a list of items when called
+var sampleServices = angular.module('sampleServices', []);
+sampleServices.service('ListOfItemsService', function () {
+    this.getList = function () {
+        // pretend call
+        return [
+            { name: "first value", number: 123 },
+            { name: "second value", number: 456 },
+            { name: "third value", number: 789 }
+        ];
+    };
+})
